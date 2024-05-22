ca8e9f658b4b9a7d545a743370a12344b359461f
ashish kalsi <ashishkalsi07@gmail.com>
2024-05-22 07:51:27 +0530

    Added Files for Hashing

---
+++ Hashing/hash.py
@@ -0,0 +1,15 @@
+# Hashing comes as an very important Data Structure when we talk about search , insert and delete operations. 
+# for a unsorted data Hashing takes O(n) for search O(1) for insert and delete operations. 
+
+# When to not use hashing is :
+# Find closest value , When data is sorted , prefix searching . For Prefiz searching we can rather use Trie Data Structure and for closest value we can use AVL Trees. 
+
+# Related Concepts - Universal Hash Function, Collision(Bounds to happen)
+
+# Adv - Perfect Hashing - Gurantees No Collision.
+
+# Handle Collision : - Chaining and Open Addressing 
+#                                         |-Linear, Qudratic and Double Hashing
+
+
+
